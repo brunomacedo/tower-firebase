189fa41b94328f71c1a2c0bce94299f5e75718ec
Bruno <contato@brunomacedo.com.br>
2017-12-23 18:55:55 -0200

    Specs introduction.

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -1,36 +1,14 @@
+// import { expect } from 'chai';
+const expect = require('chai').expect;
+
 describe('App', () => {
   describe('Method A', () => {
     context('Case 1', () => {
-      it('Should happen somenthing1.', () => {
-        // throw new Error('Just an error');
-      });
-    });
-
-    context('Case 2', () => {
-      it('Should happen somenthing2.', () => {
-        // Do something.
-      });
-
-      it('Should happen somenthing3.', () => {
-        // Do something.
-      });
-    });
-  });
-
-  describe('Method B', () => {
-    context('Case 1', () => {
-      it('Should happen somenthing1.', () => {
-        // Do something.
-      });
-    });
-
-    context('Case 2', () => {
-      it('Should happen somenthing2.', () => {
-        // Do something.
-      });
-
-      it('Should happen somenthing3.', () => {
-        // Do something.
+      it('Should have a size of 4 when push another value to the array.', () => {
+        const arr = [1, 2, 3];
+        arr.push(4);
+        // console.log(arr.length);
+        expect(arr).to.have.lengthOf(3);
       });
     });
   });
