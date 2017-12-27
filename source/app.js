8c148fe1b7fd4cd13fd300e82073153c01d0103f
Bruno <contato@brunomacedo.com.br>
2017-12-26 23:35:47 -0200

    Main file exports.

--- source/app.js
+++ source/app.js
@@ -1,3 +1,6 @@
-const nothingToDo = 'Great job!';
+const sum = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const mult = (a, b) => a * b;
+const div = (a, b) => a / b;
 
-nothingToDo();
+export { sum, sub, mult, div };
