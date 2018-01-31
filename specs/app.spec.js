db6b15d635652bb075d62d679f4052cca8264ed5
Bruno Macedo <contato@brunomacedo.com.br>
2018-01-31 08:37:23 -0200

    Cover 100% of tests

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { sum, sub, mult, div } from '../source/app';
 
-describe('Calc', () => {
+describe('Calculate', () => {
   context('Smoke tests', () => {
     it('Should exist the method `sum`', () => {
       expect(sum).to.exist;
@@ -27,6 +27,36 @@ describe('Calc', () => {
   context('Sum', () => {
     it('Should be equal 5 when sum: 2 + 3', () => {
       expect(sum(2, 3)).to.be.equal(5);
+    });
+  });
+
+  context('Subtract', () => {
+    it('Should be equal 1 when sub: 3 - 1', () => {
+      expect(sub(3, 1)).to.be.equal(2);
+    });
+
+    it('Should be equal -1 (negative value) when sub: 2 - 3', () => {
+      expect(sub(2, 3)).to.be.equal(-1);
+    });
+  });
+
+  context('Multiply', () => {
+    it('Should be equal 10 when mult: 5 x 2', () => {
+      expect(mult(5, 2)).to.be.equal(10);
+    });
+
+    it('Should be equal 0 when mult: any x 0', () => {
+      expect(mult(121, 0)).to.be.equal(0);
+    });
+  });
+
+  context('Divide', () => {
+    it('Should be equal 10 when div: 100 / 10', () => {
+      expect(div(100, 10)).to.be.equal(10);
+    });
+
+    it('Should be equal 2.5 when div: 5 / 2', () => {
+      expect(div(5, 2)).to.be.equal(2.5);
     });
   });
 });
